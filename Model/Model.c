a30904f7d53366eb46853cce38c6783abeda8419
KamilSuchanek95 <34289315+KamilSuchanek95@users.noreply.github.com>
2018-07-11 08:36:55 +0200

    Add files via upload

--- Model/Model.c
+++ Model/Model.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include "Kalman.h"
 #include "PID.h"
-
-
-
-//#define M_E			45
-/*Budynek posiada pojemność cieplną -> ilość ciepła potrzebna by zmienić temperaturę wewnątrz o stopień*/
-/*C [ J/K = (W*s)/K]*/
-/**/
-/*Moc grzałki jest stała, podlega załączeniu.*/
-/*Wyłączenie spowoduje spadek generowanego przezeń strumienia ciepła [J]*/
-/*Zależność opisująca spadek mocy grzałki o wartości początkowej P0:*/
-/*P(t) = P0 * exp(t/(R*C)) */
 
 /*=====================================================================================================*/
 /*Parametry początkowe:*/
@@ -26,7 +14,7 @@ typedef struct{
 	float Pow;	/*Powierzchnia umowna budynku => [ m^2 ]*/
 	float U; 	/*Przenikalność temperaturowa budynku => [ W/(m^2 * K) ]*/
 }model_st_t;
-model_st_t model_st={25, -5, 3150,1, 80000,400,0.085};
+model_st_t model_st={25, -5, 3150,1, 1500,400,0.085};
 model_st_t* m = &model_st;
 /*=====================================================================================================*/
 int main(){
@@ -38,38 +26,35 @@ int main(){
 	float Zs; /* Zysk temperatury wewnętrznej z grzałki */
 	float dT; /* Różnica temperatur */
 	float Ptot; /* Strumień ciepła do zewnątrz */
-	float P = 1; /* Aktualne ciepło grzałki */
+	float P = 0; /* Aktualne ciepło grzałki */
 	float R = pow(m->U, -1);
 	/*Pętla główna*/
-	int i; for(i = 0; i<10000; i++)/* t */
+	int i; for(i = 0; i<3600; i++)/* t */
 	{
-
-		if(i>5000){m->G=0;};
+		if(i>2400){m->G=0;};
 
 		if(m->G == 1)/* Włączenie grzałki -> P(t) = Pb*exp(t/(R*C)) */
 				{
-					if (P >= m->Pg){/*Wartość już nie rośnie*/}else{P = m->Pg * exp(-i/(R*m->U));}/* Nagrzewanie grzałki */
+					if (P >= m->Pg){/*Wartość już nie rośnie*/}else{P = m->Pg * exp(i/(R*m->C));}/* Nagrzewanie grzałki */
 					dT = abs(m->Tw - m->Tz); /* Ustalenie różnicy temperatur */
-					Ptot = m->U * m->Pow * (m->Tw - m->Tz);/* Ciepło uchodzące z budynku */
+					Ptot = m->U * m->Pow * dT;/* Ciepło uchodzące z budynku */
 					Zs = (P-Ptot) / m->C;		/* temperatura zapewniona przez ciepło grzałki */
 					m->Tw = m->Tw + Zs; /* Zmiana temperatury wewnątrz */
 				}
 				else/* G != 1 */
 				{
-					if (P <= 0.1){/*Wartość już nie maleje*/}else{P = m->Pg *(1 - exp(-i/(R*m->U)));}/* Ochładzanie grzałki */
+					if (P <= 0.1){/*Wartość już nie maleje*/}else{P = m->Pg *(1 - exp(-i/(R*m->C)));}/* Ochładzanie grzałki */
 					dT = abs(m->Tw - m->Tz); /* Ustalenie różnicy temperatur */
-					Ptot = m->U * m->Pow * (m->Tw - m->Tz);/* Ciepło uchodzące z budynku */
+					Ptot = m->U * m->Pow * dT;/* Ciepło uchodzące z budynku */
 					Zs = (P-Ptot) / m->C;		/* temperatura zapewniona przez ciepło grzałki */
 					m->Tw = m->Tw + Zs; /* Zmiana temperatury wewnątrz */
 				}
-	//if(m->G){fprintf(fp,"%d\n", 1);}else{fprintf(fp,"%d\n", 1);}
-		fprintf(fp, "%d %f\n",i, m->Tw);
+		 fprintf(fp, "%d %f\n",i, m->Tw);
 	}
 	fclose(fp);
 	system("gnuplot Rysowanie.gp -p");
 	return 0;
 }
-
 
 
 
---
+++ Model/Release/Default/subdir.mk
@@ -0,0 +1,13 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../Default/Kalman.o \
+../Default/Model.o \
+../Default/PID.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
